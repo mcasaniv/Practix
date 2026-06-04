f3ab2b2edda66ce9e8df9dd3c53a235796df5551
casaniv <cathe2118@gmail.com>
2026-06-03 21:13:00 -0500

    Fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-    base: "/academia-ugarte/",
+    base: "/Academia-Ugarte/",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
